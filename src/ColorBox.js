363f7c304fc9fc3d59a854b44ad57823f628bf92
Gagan Singh <gagansingh.dev@outlook.com>
2020-08-27 15:03:09 -0400

    Added animation for copy overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
-export default class ColorBox extends Component {
+class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { copied: false };
+    }
+    changeCopyState = () => {
+        this.setState({ copied: true }, () => {
+            setTimeout(() => this.setState({ copied: false }), 1500);
+        });
+    };
+
     render() {
-        // Destructure props
+        // Destructure props and state
         const { name, background } = this.props;
+        const { copied } = this.state;
 
         return (
-            <CopyToClipboard text={background}>
-                <div className="ColorBox" style={{ background }}>
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div style={{ background }} className="ColorBox">
+                    <div
+                        style={{ background }}
+                        className={`copy-overlay ${copied && "show"}`}
+                    />
+                    <div className={`copy-msg ${copied && "show"}`}>
+                        <h1>copied!</h1>
+                        <p>{this.props.background}</p>
+                    </div>
                     <div className="copy-container">
                         <div className="box-content">
                             <span>{name}</span>
@@ -23,3 +42,4 @@ export default class ColorBox extends Component {
         );
     }
 }
+export default ColorBox;
